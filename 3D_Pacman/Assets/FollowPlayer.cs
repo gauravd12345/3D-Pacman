f23b85e54a647be3c6a33f87ef1b5d1d8b33e0ab
gauravd12345 <gauravdharmadhikari2@gmail.com>
2023-07-15 12:39:52 -0700

    Created base project
    - Added gameObjects and assets
    - Created TutorialMap Scene
    - Created scripts for player movement and camera follow

---
+++ 3D_Pacman/Assets/FollowPlayer.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 cameraPos;
+    
+    
+    private float horizontalInput;
+    private float verticalInput;
+
+    // Update is called once per frame
+    void Start() {
+
+        // Reset's the camera's position 
+        transform.position = new Vector3(0, 0, 0);
+        cameraPos.y -= player.position.y;
+
+
+        
+    }
+
+
+    void Update(){
+        GetInput();
+        
+
+        Vector3 p_x = player.forward * cameraPos.z;
+        Vector3 p_z = player.right * cameraPos.x;
+        
+        Vector3 localOffset = (p_x + p_z) + new Vector3(0, cameraPos.y, 0);
+
+
+        transform.position = player.position + localOffset;
+        transform.rotation = player.rotation;
+
+    }
+
+    private void GetInput(){
+        horizontalInput = Input.GetAxis("Horizontal");
+
+
+    }
+
+    
+}
---
+++ 3D_Pacman/Assets/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Transform camera;
+
+    public float sensitivity;
+    public float revolutionTime;
+    public float maxRotationAngle;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float rotationVal;
+
+    // Update is called once per frame
+    void Update(){
+
+        // Gets the horizontal and vertical inputs
+        GetInput();
+
+        Vector3 c_z;
+        Vector3 c_x = camera.forward * verticalInput;
+        if (verticalInput != 0)
+        {
+            c_z = camera.right * horizontalInput * Mathf.Cos(camera.eulerAngles.y * Mathf.Deg2Rad);
+
+        }
+        else{
+            c_z = new Vector3(0, 0, 0);
+        }
+
+        transform.position += (c_x + c_z) * sensitivity;
+        
+        float rotationCheck = horizontalInput * 360 * Time.deltaTime / revolutionTime;
+        rotationVal += rotationCheck;
+
+        
+        Vector3 rotationVector = new Vector3(0, rotationVal, 0);
+        transform.rotation = Quaternion.Euler(rotationVector);
+        
+        
+        
+    }
+
+    private void GetInput(){
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    
+}
